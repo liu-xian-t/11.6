f433f801c5f67b89c8a53f5185f9950e2704adf2
liu-xtong <2576778991@qq.com>
2019-11-14 23:00:32 +0800

    dva改进

--- dav/src/index.js
+++ dav/src/index.js
@@ -7,11 +7,11 @@ const app = dva();
 // 2. Plugins
 // app.use({});
 
-
-app.model(require('./models/num').default);
+// 3. Model
+app.model(require('./models/gav').default);
 
 // 4. Router
-app.router(require('./router').default);
+app.router(require('./routes/index').default);
 
 // 5. Start
 app.start('#root');
--- dav/src/view/indexPage.js
+++ dav/src/view/indexPage.js
@@ -1,42 +1,45 @@
-import React, { Component } from 'react';
-import {connect} from 'dva'
-import styles from "./indexPage.css"
-export class indexPage extends Component {
-    btnClick(e){
-        console.log('e...',e,this)
-    }
-    render() {
-        let {changeNum} =this.props
-        return (
-            <div className={styles.normal}>
-             <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-                    <div>
-                    <button onClick={()=>changeNum('+')}>+</button>
-                      <span>{this.props.num}</span>
-                    <button onClick={()=>changeNum('-')}>-</button>
-                </div>
-           </div>
-        );
-    }
-}
-indexPage.protoTypes={
-    num:Number
-}
-indexPage.defaultProps = {
-    num: 1000
+import React from 'react';
+import { connect } from 'dva';
+import { Layout, Breadcrumb } from 'antd';
+import styles from './IndexPage.scss';
+// 引入自定义菜单
+import MyMenu from '../components/MyMenu';
+// 引入封装路由
+import RouterView from '../routes/RouterView'
+
+const { Header, Content, Sider } = Layout;
+
+
+class IndexPage extends React.Component{
+  render(){
+    console.log('this.router...', this.props);
+    return (
+      // 先上下布局
+      <Layout className={styles.wrap}>
+        <Header className="header">
+          头部
+        </Header>
+        {/* 再左右布局 */}
+        <Layout className={styles.container}>
+          <Sider width={200} className={styles.sider}>
+            <MyMenu/>
+          </Sider>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content className={styles.content}>
+              {/* <p className='sider content'>Content</p> */}
+
+              <RouterView routes={this.props.routes}/>
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    );
   }
-const mapStateToPorps=state=>{
-    console.log('state...',state);
-    return{
-        num:state.num.num
-    }
 }
-const mapDispatchToProps=dispatch=>{
-    return{
-        changeNum:type=>dispatch({
-            type:'num/changeNum',
-            payload:{type}
-        })
-    }
-}
-export default connect(mapStateToPorps, mapDispatchToProps)(indexPage);
+
+export default connect()(IndexPage);
